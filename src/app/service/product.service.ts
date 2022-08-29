c0aec99f5be1efdc55f400db705a180b92960580
Silas <silas.oliveira8@gmail.com>
2022-08-29 10:22:56 -0300

    chore: discart product services

--- src/app/service/product.service.ts
+++
@@ -1,9 +0,0 @@
-import { ProductModel } from '../models';
-
-class ProductService {
-  static async getById(id: string) {
-    const products = ProductModel.getById(id);
-  }
-}
-
-export { ProductService };
