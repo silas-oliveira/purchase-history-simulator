a6ba3bd3415975968ce5482f853693e85b53e7eb
Silas <silas.oliveira8@gmail.com>
2022-08-29 09:49:26 -0300

    chore: add interfaces

---
+++ src/interfaces/ICart.ts
@@ -0,0 +1,4 @@
+export default interface ICart {
+  purchaseInformation: [{ [key: string]: number | number[] }];
+  // eslint-disable-next-line semi
+}
---
+++ src/interfaces/IProduct.ts
@@ -0,0 +1,5 @@
+export default interface IProduct {
+  productId: number;
+  quantity: number;
+// eslint-disable-next-line semi
+}
